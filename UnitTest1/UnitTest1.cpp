2ea88a057268a8cc220e2479d17d092c78bef078
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2021-11-26 00:09:09 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 8.1.2/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char a = 'Q';
+			char b = 'S';
+			const char* str = "TestQQQ";
+			bool R = Include(str, a, b,0);
+			Assert::AreEqual(false, R);
+		}
+	};
+}
---
+++ lab 8.1.2/Source.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+bool Include(const char* str, const char a, const char b, int i)
+{
+    if ((a == str[i] && b == str[i + 1]) ||
+        (b == str[i] && a == str[i + 1]))
+        return 1;
+    if (str[i] != '\0')
+        return Include(str, a, b, i + 1);
+    else
+        return 0;
+}
+char* Replace(char* R, const char* str, char* s, int i)
+{
+    if (str[i + 1] != 0)
+    {
+        if ((str[i] == 'S' && str[i + 1] == 'Q') ||
+            (str[i] == 'Q' && str[i + 1] == 'S'))
+        {
+            strcat(s, "***");
+            return Replace(R, str, s + 3, i + 2);
+        }
+        else
+        {
+            *s++ = str[i++];
+            *s = '\0';
+            return Replace(R, str, s, i);
+        }
+    }
+    else
+    {
+        *s++ = str[i++];
+        *s++ = str[i++];
+        *s = '\0';
+        return R;
+    }
+}
+int main()
+{
+    char str[101];
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+
+    char a = 'Q';
+    char b = 'S';
+    if (Include(str, a, b, 0))
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+    char* R1 = new char[151];
+    R1[0] = '\0';
+    char* R2;
+    R2 = Replace(R1, str, R1, 0);
+    cout << "Modified string (result): " << R2 << endl;
+    return 0;
+}
